44517c4ff5c704b0ffcdeba531239d54eeaaa25a
unknown <sang.lequang97@gmail.com>
2018-11-25 21:10:22 +0700

    fix file name for image

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const multer = require('multer');
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8000;
 
 const app = express();
 app.get('/', (req, res) => {
@@ -13,14 +13,15 @@ var storage = multer.diskStorage({
         cb(null, './upload');
     },
     filename: (req, file, cb) => {
-        cb(null,Date.now()+file.originalname);
+        //console.log(file.originalname.substr(file.originalname.length - 3));
+        cb(null, `${new Date().getTime()}.${file.originalname.substr(file.originalname.length - 3)}`);
     }
 })
 var upload = multer({storage: storage});
 
 
 app.post('/upload', upload.single('avatar'), (req, res) => {
-    console.log(req.file);
+    console.log(req.body);
     res.json(req.file)
 });
 app.listen(port, ()=>console.log(`server running on port: ${port}`));
